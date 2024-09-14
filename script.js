5ad77407017c8ad35ffea721cb216fee0fae06a1
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-14 12:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+ let table = document.querySelector("#sampleTable");
+
 function insert_Row() {
     //Write your code here
-  
+	let tr = document.createElement("tr");
+	table.prepend(tr);
+	let cell1 = tr.insertCell();
+	cell1.innerText = "New Cell1"
+	let cell2 = tr.insertCell();
+	cell2.innerText = "New Cell2"
+	
   
 }
